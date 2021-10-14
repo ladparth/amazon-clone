0aae3577ef5cd267ce0ed175d4c248c4763261be
Parth Lad <parthlad3915@gmail.com>
2021-10-14 18:45:04 -0400

    Product Card Design and API Data Load

--- src/components/ProductFeed.js
+++ src/components/ProductFeed.js
@@ -2,23 +2,35 @@ import React from 'react'
 import Product from './Product'
 
 function ProductFeed({products}) {
+
+    const productCards =(i,j) => {
+      return products.slice(i,j).map(({id,title,price,description,category,image}) =>(
+        <Product 
+            key={id}
+            id={id}
+            title={title}
+            price={price}
+            description={description}
+            category={category}
+            image={image}
+            // rating={rating}
+        />
+        ))
+    }
+
+
     return (
-        <div>
-        
-            {
-                products.map(({id,title,price,description,category,image}) =>(
-                    <Product 
-                        key={id}
-                        id={id}
-                        title={title}
-                        price={price}
-                        description={description}
-                        category={category}
-                        image={image}
-                        // rating={rating}
-                    />
-                ))
-            }
+
+
+        <div className="grid grid-flow-row-dense md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 md:-mt-52 mx-auto">
+            
+            { productCards(0,4)}
+            
+            <img className="md:col-span-full cursor-pointer" src="https://links.papareact.com/dyz" alt="" />
+
+            <div className="md:col-span-2">{productCards(4,5)}</div> 
+            {productCards(5,products.length)}
+            
         </div>
     )
 }
